fd80626e4db461756ae94882f21a128ce9a628d2
Gary Button <gary@forwardpartners.com>
2020-01-30 17:52:30 +0000

    Use python functool lru_cache

--- zinnia/flags.py
+++ zinnia/flags.py
@@ -1,15 +1,16 @@
 """Comment flags for Zinnia"""
-from django.contrib.auth import get_user_model
-from django.utils.lru_cache import lru_cache
+from functools import lru_cache
 
+from django.contrib.auth import get_user_model
 from zinnia.settings import COMMENT_FLAG_USER_ID
+
 
 PINGBACK = 'pingback'
 TRACKBACK = 'trackback'
 FLAGGER_USERNAME = 'Zinnia-Flagger'
 
 
-@lru_cache(1)
+@lru_cache(maxsize=1)
 def get_user_flagger():
     """
     Return an User instance used by the system
